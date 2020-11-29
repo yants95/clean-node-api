ba18bfba07287ae8c900cebd0dd35dc638cbeb97
Yan Soares <yts.1995@gmail.com>
2020-11-29 20:56:29 -0300

    test: ensure LoadSurveys route returns 403 if no accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -76,4 +76,12 @@ describe('Survey Routes', () => {
         .expect(204)
     })
   })
+
+  describe('GET /surveys', () => {
+    test('should return 403 on load surveys without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
+  })
 })
