1cdca4affdf2723d1541e25a38912a7af4aa0463
Yan Soares <yts.1995@gmail.com>
2020-11-28 23:16:50 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,18 @@
+import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+
+jest.mock('../../../../validation/validators/validation-composite')
+
+describe('AddSurvey Validation Factory', () => {
+  test('should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
